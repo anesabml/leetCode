7c5b3abb9c764dd46765053865d992fa1acf8f48
anesabml <anesabismail@gmail.com>
2020-06-03 10:56:50 +0100

    Valid parentheses

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ LeetCode Kotlin solutions
 2. [Contains duplicate](https://leetcode.com/problems/contains-duplicate/) 
 3. [Max profit](https://leetcode.com/problems/best-time-to-buy-and-sell-stock/) 
 4. [Valid anagram](https://leetcode.com/problems/valid-anagram/) 
+4. [Valid parentheses](https://leetcode.com/problems/valid-parentheses/) 
---
+++ src/ValidParentheses.kt
@@ -0,0 +1,68 @@
+import java.util.*
+
+
+class ValidParentheses {
+
+    /** Using Stack
+     * Initialize a stack S.
+     * Process each bracket of the expression one at a time.
+     * If we encounter an opening bracket, we simply push it onto the stack.
+     * This means we will process it later, let us simply move onto the sub-expression ahead.
+     * If we encounter a closing bracket, then we check the element on top of the stack.
+     * If the element at the top of the stack is an opening bracket of the same type, then we pop it off the stack and continue processing.
+     * Else, this implies an invalid expression.
+     * In the end, if we are left with a stack still having elements, then this implies an invalid expression.
+     */
+    fun isValid(s: String): Boolean {
+        val mappings =
+            hashMapOf(
+                ')' to '(',
+                '}' to '{',
+                ']' to '['
+            )
+        val stack = Stack<Char>()
+        for (c in s) {
+            if (mappings.containsKey(c)) {
+                val element = if (stack.empty()) '#' else stack.pop()
+                if (mappings[c] != element) return false
+            } else {
+                stack.push(c)
+            }
+        }
+        return stack.empty()
+    }
+
+    fun isValid2(s: String): Boolean {
+        val map = hashMapOf(
+            '(' to ')',
+            '{' to '}',
+            '[' to ']'
+        )
+        val stack = Stack<Char>()
+        for (c in s) {
+            if (c in map.keys) {
+                stack.push(map[c])
+            } else if (stack.empty() || stack.pop() != c) {
+                return false
+            }
+        }
+        return stack.empty()
+    }
+}
+
+fun main() {
+    val input = "{[]}"
+
+    val validParentheses = ValidParentheses()
+
+    val firstStartTime = System.currentTimeMillis()
+    println(validParentheses.isValid(input))
+    val firstEndTime = System.currentTimeMillis()
+
+    val secondStartTime = System.currentTimeMillis()
+    println(validParentheses.isValid2(input))
+    val secondEndTime = System.currentTimeMillis()
+
+    println("First Solution execution time: ${firstEndTime - firstStartTime}")
+    println("Second Solution execution time: ${secondEndTime - secondStartTime}")
+}
