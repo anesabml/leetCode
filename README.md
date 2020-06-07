2d9227fdf93187a63b03d363ca5bb51af9b4ea69
anesabml <anesabismail@gmail.com>
2020-06-07 10:06:23 +0100

    Three sum

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ LeetCode Kotlin solutions
 2. [Contains duplicate](https://leetcode.com/problems/contains-duplicate/) 
 3. [Max profit](https://leetcode.com/problems/best-time-to-buy-and-sell-stock/) 
 4. [Valid anagram](https://leetcode.com/problems/valid-anagram/) 
-5. [Product of Array Except Self](https://leetcode.com/problems/product-of-array-except-self/) 
-6. [Maximum Subarray](https://leetcode.com/problems/maximum-subarray/) 
+5. [Product of array except self](https://leetcode.com/problems/product-of-array-except-self/) 
+6. [Maximum subarray](https://leetcode.com/problems/maximum-subarray/) 
+7. [Three sum](https://leetcode.com/problems/3sum/) 
---
+++ src/ThreeSum.kt
@@ -0,0 +1,74 @@
+import java.util.*
+
+class ThreeSum {
+
+    /**
+     * Fix the first number and look for the other two by using the two sum algorithm (map)
+     * Time complexity : O(n2)
+     * Space complexity : O(n)
+     */
+    fun threeSum(nums: IntArray): List<List<Int>> {
+        val map = HashMap<Int, Int>()
+        for (i in nums.indices) {
+            map[nums[i]] = i
+        }
+        val result = mutableSetOf<List<Int>>()
+        for (i in nums.indices) {
+            val sum = 0 - nums[i]
+            for (j in i + 1 until nums.size) {
+                val y = sum - nums[j]
+                if (map.containsKey(y) && map[y] != j && map[y] != i) {
+                    result.add(listOf(nums[i], nums[j], y).sorted())
+                }
+            }
+        }
+        return result.toList()
+    }
+
+    /**
+     * Fix the first number and look for the other two by using the two sum algorithm
+     * Time complexity : O(n2)
+     * Space complexity : O(1)
+     */
+    fun threeSum2(nums: IntArray): List<List<Int>> {
+        nums.sort()
+        val result = mutableSetOf<List<Int>>()
+        for (i in 0 until nums.size - 2) {
+            var low = i + 1
+            var high = nums.size - 1
+            while (low < high) {
+                val sum = nums[i] + nums[low] + nums[high]
+                when {
+                    sum == 0 -> {
+                        result.add(listOf(nums[i], nums[low], nums[high]))
+                        low++
+                        high--
+                    }
+                    sum < 0 -> {
+                        low++
+                    }
+                    else -> {
+                        high--
+                    }
+                }
+            }
+        }
+        return result.toList()
+    }
+}
+
+fun main() {
+    val threeSum = ThreeSum()
+    val input = intArrayOf(-1, 0, 1, 2, -1, -4)
+
+    val firstStartTime = System.currentTimeMillis()
+    println(threeSum.threeSum(input))
+    val firstEndTime = System.currentTimeMillis()
+
+    val secondStartTime = System.currentTimeMillis()
+    println(threeSum.threeSum2(input))
+    val secondEndTime = System.currentTimeMillis()
+
+    println("First Solution execution time: ${firstEndTime - firstStartTime}")
+    println("Second Solution execution time: ${secondEndTime - secondStartTime}")
+}
